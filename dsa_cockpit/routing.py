8a0384665785bb31e6cd468ae7f20da15169a2ce
Michael Luther <michel.luther@gmail.com>
2018-11-23 18:40:57 +0100

    different paths for different websocket endpoints

--- dsa_cockpit/routing.py
+++ dsa_cockpit/routing.py
@@ -1,8 +1,14 @@
 from channels.routing import route
-from dsa_starter.consumers import ws_message, ws_connect, ws_disconnect
+from dsa_starter.consumers import message_to_heroes, connect_to_heroes, disconnect_from_heroes, connect_to_remoteControl_sender, connect_to_remoteControl_receiver, disconnect_from_remoteControl_receiver, disconnect_from_remoteControl_sender, message_to_remote_control_receivers
 
 channel_routing = [
-    route("websocket.connect", ws_connect),
-    route("websocket.receive", ws_message),
-    route("websocket.disconnect", ws_disconnect),
+    route("websocket.connect", connect_to_heroes, path=r"^/heroes"),
+    route("websocket.receive", message_to_heroes, path=r"^/heroes"),
+    route("websocket.disconnect", disconnect_from_heroes, path=r"^/heroes"),
+    route("websocket.connect", connect_to_remoteControl_receiver, path=r"^/remoteControlReceiver"),
+    route("websocket.connect", connect_to_remoteControl_sender, path=r"^/remoteControlSender"),
+    route("websocket.receive", message_to_remote_control_receivers, path=r"^/remoteControlSender"),
+    route("websocket.disconnect", disconnect_from_remoteControl_sender, path=r"^/remoteControlSender"),
+    route("websocket.disconnect", disconnect_from_remoteControl_receiver, path=r"^/remoteControlReceiver"),
+
 ]
--- dsa_starter/consumers.py
+++ dsa_starter/consumers.py
@@ -5,24 +5,51 @@ import json
 from channels import Group
 
 
-def ws_connect(message):
-    print("Connected, HAHAHAH \n")
+def connect_to_heroes(message):
+    print(message.name)
+    print("Connected to life service \n")
     message.reply_channel.send({"accept": True})
     Group("heroes").add(message.reply_channel)
 
 
-def ws_message(message):
+def message_to_heroes(message):
     # Make standard HTTP response - access ASGI path attribute directly
-
     data = json.loads(message.content['text'])
-
     character = Character.objects.get(pk=data["heroId"])
     character.life_lost = character.life_lost - data["value"]
     character.save()
-
     Group("heroes").send({
         "text": message.content['text'],
     })
 
-def ws_disconnect(message):
+
+def disconnect_from_heroes(message):
     Group("heroes").discard(message.reply_channel)
+
+
+def connect_to_remoteControl_receiver(message):
+    print(message.name)
+    print("Connected to remoteControl-Receiver service \n")
+    message.reply_channel.send({"accept": True})
+    Group("remoteControlReceiver").add(message.reply_channel)
+    
+    
+def connect_to_remoteControl_sender(message):
+    print(message.name)
+    print("Connected to remote control service \n")
+    message.reply_channel.send({"accept": True})
+    Group("remoteControlSender").add(message.reply_channel)
+
+def disconnect_from_remoteControl_receiver(message):
+    Group("remoteControlSender").discard(message.reply_channel)
+
+def disconnect_from_remoteControl_sender(message):
+    Group("remoteControlSender").discard(message.reply_channel)
+
+
+def message_to_remote_control_receivers(message):
+    # Make standard HTTP response - access ASGI path attribute directly
+    data = json.loads(message.content['text'])
+    Group("remoteControlReceiver").send({
+        "text": message.content['text'],
+    })
