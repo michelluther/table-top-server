b34db8b5b7362656fe70e5acdc56c99adf0ff9fa
Michel Luther <luther@lutherundwinter.de>
2024-01-31 01:33:44 +0100

    Installation steps 01

--- dsa_cockpit/wsgi.py
+++ dsa_cockpit/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dsa_cockpit.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dsa_cockpit.dsa_cockpit.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dsa_cockpit.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dsa_cockpit.dsa_cockpit.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
